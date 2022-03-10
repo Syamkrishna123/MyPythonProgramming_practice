694d9843dfc4d66a0a7909900b8f58202f459324
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-10 19:41:56 +0530

    Add files via upload

---
+++ ipt-optfun.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 19:05:37 2022
+
+@author: elect
+"""
+#to read and display your name
+name=input ('enter your name:\n')
+print('wow',name,'your name is very nice\n'*2)
