645aed90b36ed2adc05a9aaeaa907b0b915fdd20
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-12 12:02:52 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 06:52:56 2022
+
+@author: syamk
+"""
+
+student=('syam',12,2021)
+print(student)
+print(student[0])
+print(len(student))
+print(student.count('syam'))
+#'tuple' object does not support item assignment
+student[0]='elonmusk'
