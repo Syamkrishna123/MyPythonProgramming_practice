5167b5792e6a5507de689583076036ae40f7e52a
syamkrishna123 <syamkrishna870@gmail.com>
2022-03-14 18:19:34 +0530

    my commit

---
+++ ArithmeticOperation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 16:02:15 2022
+
+@author: elect
+"""
+#addition operation
+balls = 5
+bats = 4
+football =2
+tennis_ball = 10
+print("total number of sports items you have:",balls+bats+football+tennis_ball)
+#subtraction
+print("total number of tennis ball after one got damaged:",tennis_ball-1)
+#multiplication
+football_cost= 900
+print("price for the total football:",football*football_cost)
+#divison
+total_tennis_ball= 200
+print("cost of a tennis ball:",total_tennis_ball/tennis_ball)
+#exponent
+i=int(input('enter the number you want to find the cube of:'))
+print(i**3)
+#floor divison
+print(5//2)
+#modulus
+print(5%2)
