bfe002c9f674d8e46da9fc798fe0a0685f5c272b
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-15 14:27:29 +0530

    bitwise operators

---
+++ BitwiseOperators.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 13:43:51 2022
+
+@author: elect
+"""
+
+i=50 #110010
+j=30 #11110
+#bitwise and
+print(i&j) #010010 =18
+#bitwise or
+print(i|j) #111110 =62
+#bitwise not
+print(~i) #one's complement -(110011)= -51
+#bitwise XOR
+print(i^j)#101100=44
+#bitwise right shift
+print(i>>3) #110=6
+#bitwise left shift
+print(i<<2) #11001000 = 200
