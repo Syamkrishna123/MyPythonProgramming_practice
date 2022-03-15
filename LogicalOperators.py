83d3092f755933436c94b1556ef249875f60f4ed
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-15 12:40:16 +0530

    LogicaOperators added

---
+++ LogicalOperators.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 12:25:32 2022
+
+@author: elect
+"""
+
+i=9
+j=5
+#and
+print(i>j and i!=j)#output True
+print(i<j and i!=j)#output False
+#or
+print(i>j or i==j)#True
+#not
+print(not i>j)#False
