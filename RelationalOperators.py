9600ef6dcd1ad4ce5ecf83ab1e3182f9ffffa295
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-15 12:22:57 +0530

    Create RelationalOperators.py

---
+++ RelationalOperators.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 12:12:44 2022
+
+@author: elect
+"""
+
+#greater than
+i= 9
+j= 5
+print(i>j) #output: True
+#less than
+print(i<j) #output: False
+#equal to
+print(i==j) #output:False
+#Not equal to
+print(i!=j) #output: True
+#Greater than or equal to
+print(i>=j) #output: True
+#less than or equal to
+print(i<=j) #output: false
