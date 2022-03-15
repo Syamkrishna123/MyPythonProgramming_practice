d8fe50b63961c17ffae120558a8284a30fda291c
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-15 15:53:52 +0530

    assignment

---
+++ assignmentOperators.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 15:49:11 2022
+
+@author: elect
+"""
+
+i=0#simple assignment
+i+=1 #this is equalant to i=i+1 and returns value 1 to i
+print(i)
+i-=1
+print(i)
+j=2
+j *= 2 #2*2
+print(j)
+p=6
+p /= 3
+print(p)
+#there are other operators
