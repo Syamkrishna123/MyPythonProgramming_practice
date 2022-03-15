cefedad66f346cddfd5d93c0759a1764c9e4315d
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-15 15:43:29 +0530

    membership

---
+++ MembershipOperators.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 15:39:03 2022
+
+@author: elect
+"""
+
+l=['syam','devika','vidya','bhavya']
+#in
+print('syam' in l)#True
+#not in
+print('syam' not in l)#False
