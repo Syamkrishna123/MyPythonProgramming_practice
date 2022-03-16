e5b1f338f0aade26188bd862dc961c8b678d7a1f
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-16 13:21:35 +0530

    while

---
+++ while.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 13:14:01 2022
+
+@author: elect
+"""
+
+i=int(input('enter the limit:'))
+sum=0
+k=1
+while k<=i:
+    sum+=k
+    k+=1
+print('sum is =',sum)
+    
