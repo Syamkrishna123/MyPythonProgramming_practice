7ae73d4c64c839778e71666b62839dbcb70ef67e
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-16 15:40:05 +0530

    week2last

---
+++ breakcontinue.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 13:32:40 2022
+
+@author: elect
+"""
+
+#break
+for val in 'syam':
+    if val=='a':
+        break
+    print(val)
+#continue
+for g in 'devika':
+    if g=='i':
+        continue
+    print(i)
---
+++ function.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 15:24:52 2022
+
+@author: elect
+"""
+def mul(i):
+    for j in range(1,11):
+        print(i,'*',j,'=',i*j)
+i=int(input('enter the number:'))
+mul(i)   
