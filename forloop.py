486988a232fb678e0d95751cce2978d6cddafcd1
Syamkrishna123 <44013945+Syamkrishna123@users.noreply.github.com>
2022-03-16 13:04:51 +0530

    for

---
+++ forloop.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 12:44:31 2022
+
+@author: elect
+"""
+
+#for loop
+family={'syam':25,'devika':23,'vipin':30,'surendran':62,'chandrika':54}
+for (key, value) in family.items():
+    if value<=30:
+        print('younger generation:',key)
+    else:
+        print('older generation:',key)
+
+              
+
+        
+        
